4f0827e8eeb2f159f2b5e17b86e38cb74188e2b1
Chandni Agarwal <agarwalc@microsoft.com>
2021-10-03 17:26:31 +0530

    Reaction data map

--- Source/CompanyCommunicator/Repositories/Extensions/ReactionDataRepositoryExtensions.cs
+++ Source/CompanyCommunicator/Repositories/Extensions/ReactionDataRepositoryExtensions.cs
@@ -27,10 +27,11 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Repositories.Extensions
             string reaction,
             IMessageReactionActivity activity)
         {
-            var reactionDataEntity = ReactionDataRepositoryExtensions.ParseReactionData(reaction, activity);
+            var reactionDataEntity = ReactionDataRepositoryExtensions.ParseReactionData(reaction, activity
+                );
             if (reactionDataEntity != null)
             {
-                await reactionDataRepository.CreateAsync(reactionDataEntity);
+                await reactionDataRepository.CreateOrUpdateAsync(reactionDataEntity);
             }
         }
 
@@ -38,7 +39,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Repositories.Extensions
         /// Remove channel data in table storage.
         /// </summary>
         /// <param name="reactionDataRepository">The reaction data repository.</param>
-        /// <param name="reaction">Reaction of User.</param>
+        /// <param name="reaction"></param>
         /// <param name="activity">Bot conversation update activity instance.</param>
         /// <returns>A task that represents the work queued to execute.</returns>
         public static async Task RemoveReactionDataAsync(
@@ -49,8 +50,8 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Repositories.Extensions
             var reactionDataEntity = ReactionDataRepositoryExtensions.ParseReactionData(reaction, activity);
             if (reactionDataEntity != null)
             {
-                var found = await reactionDataRepository.GetAsync(ReactionDataTableNames.ReactionDataPartition, activity.ReplyToId);
-                if (found != null && found.Name == activity?.From?.AadObjectId)
+                var found = await reactionDataRepository.GetAsync(activity.ReplyToId, activity.From.Id);
+                if (found != null)
                 {
                     await reactionDataRepository.DeleteAsync(found);
                 }
@@ -63,8 +64,8 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Repositories.Extensions
             {
                 var reactionsDataEntity = new ReactionDataEntity
                 {
-                    PartitionKey = ReactionDataTableNames.ReactionDataPartition,
-                    RowKey = activity.ReplyToId,
+                    PartitionKey = activity.ReplyToId,
+                    RowKey = activity.From.Id,
                     ConversationID = activity.Conversation.Id,
                     ReactionId = activity.ReplyToId,
                     Name = activity?.From?.AadObjectId,
